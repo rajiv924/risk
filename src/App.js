7596f690caf086491487eb5e6bd431116d043a72
rajiv924 <rajivcse2009@gmail.com>
2022-09-27 23:21:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img src="/logo12.png "/>
+          </div>
+        
+        <link href="/dist/output.css" rel="stylesheet"/>
+          
+      </header>
+      <body>
+        <main>
+          hello this is main content here goes ???
+
+
+          <section>
+          <div class="container my-5 z-depth-1">                   
+<section class="dark-grey-text">
+
+  <div class="row pr-lg-5">
+    <div class="col-md-7 mb-4">
+
+      <div class="view">
+        <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="smaple image"/>
+      </div>
+    </div>
+    <div class="col-md-5 d-flex align-items-center">
+      <div>
+        
+        <h3 class="font-weight-bold mb-4">Material Design Blocks</h3>
+
+        <p>Lorem ipsum dolor sit amet consectetur adip elit. Maiores deleniti explicabo voluptatem quisquam nulla asperiores aspernatur aperiam voluptate et consectetur minima delectus, fugiat eum soluta blanditiis adipisci, velit dolore magnam.</p>
+
+        <button type="button" class="btn btn-orange btn-rounded mx-0">Download</button>
+
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+
+
+
+          </section>
+        </main>
+        </body>
+        <footer className='App-footer'>
+        </footer>
+    </div>
+  )
+}
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
